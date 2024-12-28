2150edbf1d48d3d3ef9910c656104a729475392e
Cakeblade <jch1070@gmail.com>
2024-12-29 03:07:44 +0900

    Add bat file can make new env file

---
+++ run_newenv.bat
@@ -0,0 +1,14 @@
+@echo off
+set VENV_PATH=.\.venv\Scripts\Activate.ps1
+
+if exist "%VENV_PATH%" (
+    powershell.exe -ExecutionPolicy Bypass -File "%VENV_PATH%"
+    powershell -Command "&{python main.py --setenv True;}"
+) else (
+    powershell -Command "&{python -m venv .venv;}"
+    powershell.exe -ExecutionPolicy Bypass -File "%VENV_PATH%"
+    powershell -Command "&{pip install -r requirements.txt;}"
+    powershell -Command "&{python main.py --setenv True;}"
+)
+
+pause
